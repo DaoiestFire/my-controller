97a61d0218d834af53dec6a1b2fe41aec9280db0
ljw <liujingwei1997@outlook.com>
2023-06-10 16:13:09 +0800

    完成mycontroller

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # MyController
 
 #### 介绍
+
 基于client-go实现对k8s集群的访问，这个简易的controller对新创建的pod增加一行annotation
 
 1. v0.1.0: 实现使用证书实例化RESTClient并访问apiserver，打印环境上kube-system命名空间下的pod
 2. v0.1.1: 实现使用证书实例化ClientSet并访问apiserver，打印环境上kube-system命名空间下的pod和kube-flannel下的ds
-3. v1.0.0: 实现一个简易的controller，当pod增加时，打印增加事件，并将pod增加到workqueue，仅处理携带label"mycontroller:mycontroller"的pod。
-controller更新pod，为pod增加一个新label"mycontroller:processed"。接受删除事件，打印到控制台。
+3. v1.0.0: 实现一个简易的controller，当pod增加时，打印增加事件，并将pod增加到workqueue，仅处理携带label"mycontroller:
+   mycontroller"的pod。
+   controller更新pod，为pod增加一个新label"processed:processed"。接受删除事件，打印到控制台。
 
 #### 使用教程
 
 ##### V0.1.0
+
 ```shell
 cd cmd
 go mod tidy
@@ -18,10 +21,12 @@ go build -o mc
 # 指定自己的apiserver的ip与端口，指定证书与密钥
 ./mc --ca-file /opt/kubernetes/ssl/ca.pem --cert-file /opt/kubernetes/ssl/admin.pem --key-file /opt/kubernetes/ssl/admin-key.pem --host 10.0.4.15 --port 6443
 ```
+
 可以看到环境上的pod已经打印出来了
 ![](images/v0.1.png)
 
 ##### V0.1.1
+
 ```shell
 cd cmd
 go mod tidy
@@ -29,6 +34,7 @@ go build -o mc
 # 指定自己的apiserver的ip与端口，指定证书与密钥
 ./mc --ca-file /opt/kubernetes/ssl/ca.pem --cert-file /opt/kubernetes/ssl/admin.pem --key-file /opt/kubernetes/ssl/admin-key.pem --host 10.0.4.15 --port 6443
 ```
+
 可以看到环境上的pod与ds已经打印出来了
 ![](images/v0.1.1.png)
 
--- pkg/controller/my_controller.go
+++ pkg/controller/my_controller.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"time"
 
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/util/wait"
 	coreinformers "k8s.io/client-go/informers/core/v1"
 	clientset "k8s.io/client-go/kubernetes"
@@ -36,6 +39,13 @@ func NewMyController(podInformer coreinformers.PodInformer, kubeclient clientset
 		UpdateFunc: mc.updatePod,
 		DeleteFunc: mc.deletePod,
 	})
+
+	selector := &metav1.LabelSelector{
+		MatchLabels: map[string]string{"mycontroller": "mycontroller"},
+	}
+	resultSelector, _ := metav1.LabelSelectorAsSelector(selector)
+
+	mc.selector = resultSelector
 	return mc
 }
 
@@ -44,6 +54,8 @@ type MyController struct {
 	podStoreSynced cache.InformerSynced
 	podLister      corelisters.PodLister
 	queue          workqueue.RateLimitingInterface
+
+	selector labels.Selector
 }
 
 func (mc *MyController) Run(ctx context.Context) {
@@ -62,20 +74,109 @@ func (mc *MyController) Run(ctx context.Context) {
 
 }
 
-func (mc *MyController) syncPod() error {
+func (mc *MyController) syncPod(key string) error {
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		return err
+	}
+	if len(ns) == 0 || len(name) == 0 {
+		return fmt.Errorf("invalid pod key %q: either namespace or name is missing", key)
+	}
+
+	sharedPod, err := mc.podLister.Pods(ns).Get(name)
+	if err != nil {
+		return fmt.Errorf("get pod [%s] from lister failed ---> [%v]", key, err)
+	}
+
+	// 深拷贝，避免修改缓存中的对象
+	pod := *sharedPod.DeepCopy()
+	podLabels := pod.GetLabels()
+	// 如果匹配了把processed:processed标签加上
+	if mc.isTargetPod(&pod) {
+		podLabels["processed"] = "processed"
+		fmt.Printf(infoFormat, fmt.Sprintf("pod [%v] add processed:processed label", key))
+	} else {
+		// 反之，将标签删除
+		delete(podLabels, "processed")
+		fmt.Printf(infoFormat, fmt.Sprintf("pod [%v] delete processed:processed label", key))
+	}
+	pod.SetLabels(podLabels)
+
+	_, err = mc.kubeClient.CoreV1().Pods(ns).Update(context.TODO(), &pod, metav1.UpdateOptions{})
+	if err != nil {
+		return fmt.Errorf("update pod [%v] failed", key)
+	}
+
 	return nil
 }
 
+// 新pod如果携带”mycontroller:mycontroller“标签，就需要处理，置入处理队列中
 func (mc *MyController) addPod(obj interface{}) {
+	pod := obj.(*v1.Pod)
 
+	//如果已经删除了就不处理
+	if pod.DeletionTimestamp != nil {
+		mc.deletePod(pod)
+		return
+	}
+
+	// 获取pod的key。即”namespace：pod name“
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(pod)
+	if err != nil {
+		fmt.Printf(errorFormat, fmt.Sprintf("get key for obj [%v] failed", key))
+		return
+	}
+
+	// 这里去匹配pod的labels
+	if !mc.isTargetPod(pod) {
+		fmt.Printf(errorFormat, fmt.Sprintf("[%v] is not target pod", key))
+		return
+	}
+
+	mc.queue.Add(key)
+	fmt.Printf(infoFormat, fmt.Sprintf("add pod [%v] to queue success", key))
 }
 
+// 如果一个pod原来没有”mycontroller:mycontroller“标签，后续修改其内容将标签加上了，就需要处理，置入处理队列中
+// 如果一个pod原来有”mycontroller:mycontroller“标签，现在没有了。我们要删除添加上的标签，也需要置入处理队列
 func (mc *MyController) updatePod(old interface{}, cur interface{}) {
+	oldPod := old.(*v1.Pod)
+	curPod := cur.(*v1.Pod)
+	//如果已经删除了就不处理
+	if curPod.DeletionTimestamp != nil {
+		mc.deletePod(curPod)
+		return
+	}
 
+	// 获取pod的key。即”namespace：pod name“
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(curPod)
+	if err != nil {
+		fmt.Printf(errorFormat, fmt.Sprintf("get key for obj [%v] failed", key))
+		return
+	}
+
+	isOldMatch := mc.isTargetPod(oldPod)
+	isCurMatch := mc.isTargetPod(curPod)
+
+	if (isOldMatch && isCurMatch) || (!isOldMatch && !isCurMatch) {
+		fmt.Printf(infoFormat, fmt.Sprintf("pod [%v] no need process", key))
+	}
+
+	mc.queue.Add(key)
+	fmt.Printf(infoFormat, fmt.Sprintf("add pod [%v] to queue success,isOldMatch[%v] isCurMatch [%v]", key, isOldMatch, isCurMatch))
 }
 
+// 删除pod就打印日志即可
 func (mc *MyController) deletePod(obj interface{}) {
+	pod := obj.(*v1.Pod)
+	// 获取pod的key。即”namespace：pod name“
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(pod)
+	if err != nil {
+		fmt.Printf(errorFormat, fmt.Sprintf("get key for obj [%v] failed", key))
+		return
+	}
 
+	fmt.Printf(infoFormat, fmt.Sprintf("pod [%v] is deleted", key))
 }
 
 func (mc *MyController) worker(ctx context.Context) {
@@ -90,11 +191,15 @@ func (mc *MyController) processNextWorkItem(ctx context.Context) bool {
 	}
 	defer mc.queue.Done(key)
 
-	if err := mc.syncPod(); err != nil {
-		fmt.Printf(errorFormat, "process pod [%v] failed", key)
+	if err := mc.syncPod(key.(string)); err != nil {
+		fmt.Printf(errorFormat, "process pod [%v] failed ---> [%v]", key, err)
 		return false
 	}
 
 	fmt.Printf(infoFormat, "process pod [%v] success", key)
 	return true
+}
+
+func (mc *MyController) isTargetPod(pod *v1.Pod) bool {
+	return mc.selector.Matches(labels.Set(pod.GetLabels()))
 }
